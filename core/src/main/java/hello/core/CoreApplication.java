5e7eb32a1510abedfb2cb33d99918703347432d9
남우석 <nws94@douzone.com>
2024-10-30 09:40:22 +0900

    core spring boot study

---
+++ core/src/main/java/hello/core/CoreApplication.java
@@ -0,0 +1,24 @@
+package hello.core;
+
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApplication {
+
+	public static void main(String[] args) {
+
+		Member member = new Member(1L, "홍길동", "VIP");
+
+		MemberService memberService = new MemberServiceImpl();
+
+		memberService.join(member);
+
+		System.out.println(memberService.findMember(1L).getName());
+		SpringApplication.run(CoreApplication.class, args);
+	}
+
+}
