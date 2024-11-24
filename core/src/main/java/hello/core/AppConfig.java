c0a33cfc9d91e13611c786482363616ffa8d9ca8
남우석 <nws94@douzone.com>
2024-11-25 00:16:17 +0900

    스프링 전환하기

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,21 +9,27 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     /**
      *  DIP 위배되지 않도록 impl에서 선언하지 않고 생성자 주입을 통해 관리
+     *
+     *  bean만 적어주명 name default는 메소드 네이밍을 따라간다.
      */
-
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
@@ -33,6 +39,7 @@ public class AppConfig {
      * 할인 정책 변경으로 FixDiscountPolicy -> RateDiscountPolicy 변경해야 할때 app config만 변경해주면 된다.
      * @return
      */
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
