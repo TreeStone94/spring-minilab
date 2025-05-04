b18ccd4392a5379a9c4e09bea99c6eb03efbae90
남우석 <hinyong22@gmail.com>
2025-05-05 02:45:03 +0900

    싱글톤 빈과 프로토타입 함께 사용 시 문제점

---
+++ core/src/test/java/hello/core/scope/SingletoneWithPrototypeTest1.java
@@ -0,0 +1,73 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class SingletoneWithPrototypeTest1 {
+
+	@Test
+	void prototypeFind() {
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+		PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
+		bean1.addCount();
+		Assertions.assertThat(bean1.getCount()).isEqualTo(1);
+
+		PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
+		bean2.addCount();
+		Assertions.assertThat(bean2.getCount()).isEqualTo(1);
+	}
+
+	@Test
+	void singletonClientUsePrototype() {
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class,PrototypeBean.class);
+
+		ClientBean clientBean1 = ac.getBean(ClientBean.class);
+		int count1 = clientBean1.logic();
+		Assertions.assertThat(count1).isEqualTo(1);
+
+		ClientBean clientBean2 = ac.getBean(ClientBean.class);
+		int count2 = clientBean2.logic();
+		Assertions.assertThat(count2).isEqualTo(2);
+
+	}
+
+	@Scope("singleton")
+	@RequiredArgsConstructor
+	static class ClientBean {
+		private final PrototypeBean prototypeBean;
+
+		public int logic() {
+			prototypeBean.addCount();
+			int count = prototypeBean.getCount();
+			return count;
+		}
+	}
+
+	@Scope("prototype")
+	static class PrototypeBean {
+		private int count;
+
+		public void addCount() {
+			count++;
+		}
+
+		public int getCount() {
+			return count;
+		}
+
+		@PostConstruct
+		public void init() {
+			System.out.println("PrototypeBean.init" + this);
+		}
+
+		@PreDestroy
+		public void destroy() {
+			System.out.println("PrototypeBean.destroy");
+		}
+	}
+}
